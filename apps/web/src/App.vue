e30ec80aed3a2403bc14b49ca28eb559254bffea
heavenkiller2018 <yangyong_th@qq.com>
2020-07-17 21:25:42 +0800

    1

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 
-<script lang="ts">
+  <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import ServiceWorkerUpdatePopup from '@/pwa/components/ServiceWorkerUpdatePopup.vue';
 
